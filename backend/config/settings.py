b1e9ec611d85e851ea57299d1cea6f8db6bec956
rockyffgod <rockyffgod@gmail.com>
2026-06-10 20:28:47 +0545

    Fix STATIC_URL to use leading slash for Render collectstatic

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -91,7 +91,7 @@ TIME_ZONE = 'Asia/Kathmandu'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
